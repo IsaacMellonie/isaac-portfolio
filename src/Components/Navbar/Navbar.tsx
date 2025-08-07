a499644de73d68fe870af8611ed59c0546d1d722
IsaacMellonie <izeymelon@yahoo.com>
2025-08-07 19:00:32 +1000

    created repo and made navbar

---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../../Styles/Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav id='navbarWrapper' className=''>
+      <div id='navbar'>
+        <img src="" alt="" />
+        <menu id='navMenuItems' className='menuContainer'>
+          <p className='menuItem'>About</p>
+          <p className='menuItem'>Work</p>
+          <p className='menuItem'>Contact</p>
+        </menu>
+      </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
